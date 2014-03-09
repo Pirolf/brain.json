8c5b04e270ce1b671806dd673d9c3079558b94f7
Kevin Alford <zeroeth@pixelflow.org>
2014-03-09 18:54:08 -0500

    Split json messages on carraige returns.

--- neurosky_connector.js
+++ neurosky_connector.js
@@ -4,13 +4,17 @@ var HOST = '127.0.0.1';
 var PORT = 13854;
 
 
+// TODO wrap this in a class with callbacks for handling the specific json events
+
+
 /* Network Socket to Thinkgear Connector */
 
 var client = new net.Socket();
 
 client.connect(PORT, HOST, function() {
 
-    client.write('{"appName": "brain.json", "appKey": "9f54141b4b4c567c558d3a76cb8d715cbde03096", "format": "Json"}');
+	client.write(JSON.stringify({"enableRawOutput": false, "format": "Json"}));
+    client.write(JSON.stringify({"appName": "brain.json", "appKey": "9385e00cb35898fb8f551ae9b2d0d1824aaf481e"}));
 
 	// write session start
 });
@@ -21,6 +25,17 @@ client.connect(PORT, HOST, function() {
 client.on('data', function(data) {
 	// save to database
 	
+	// For when two results come in the same packet
+	var rows = data.toString().split('\r');
+
+	// Remove trailing newline.
+	rows.pop();
+
+	// Process Rows
+	rows.forEach( function(row) {
+		console.log(JSON.parse(row.toString()));
+	});
+
 	// * normal event
 	// * blink event
 	// * lost signal event
