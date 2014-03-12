1d114ad3c6ad2997e3e8d4ac0e37562b98b507c8
Kevin Alford <zeroeth@pixelflow.org>
2014-03-11 22:13:47 -0500

    Eeg Event saving and express.js json serve!

--- neurosky_connector.js
+++ neurosky_connector.js
@@ -1,7 +1,36 @@
 var net = require('net');
+var express = require('express');
+var colors = require('colors');
 
 var HOST = '127.0.0.1';
 var PORT = 13854;
+
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+
+var EegSchema = mongoose.Schema({
+	eSense: {
+		attention:   Number,
+		meditation:  Number
+	},
+	eegPower: {
+		delta:       Number,
+		theta:       Number,
+		lowAlpha:    Number,
+		highAlpha:   Number,
+		lowBeta:     Number,
+		highBeta:    Number,
+		lowGamma:    Number,
+		highGamma:   Number
+	},
+	poorSignalLevel: Number,
+	created_at: { type: Date, default: Date.now },
+});
+
+var EegEvent = mongoose.model('EegEvent', EegSchema);
+
 
 
 // TODO wrap this in a class with callbacks for handling the specific json events
@@ -16,6 +45,7 @@ client.connect(PORT, HOST, function() {
 	client.write(JSON.stringify({"enableRawOutput": false, "format": "Json"}));
     client.write(JSON.stringify({"appName": "brain.json", "appKey": "9385e00cb35898fb8f551ae9b2d0d1824aaf481e"}));
 
+    console.log('ThinkGear Connector'.magenta +' on '.grey + '%s:%d'.magenta, HOST, PORT);
 	// write session start
 });
 
@@ -33,7 +63,19 @@ client.on('data', function(data) {
 
 	// Process Rows
 	rows.forEach( function(row) {
-		console.log(JSON.parse(row.toString()));
+		var json = JSON.parse(row.toString());
+
+		if(json.eSense) {
+			var eeg_event = new EegEvent(json);
+			eeg_event.save(function (error, model) {
+				if (error) {
+					return console.error(err.red);
+				}
+				else {
+					// console.log("<", json, model);
+				}
+			});
+		}
 	});
 
 	// * normal event
@@ -62,4 +104,33 @@ client.on('data', function(data) {
 // Add a 'close' event handler for the client socket
 client.on('close', function() {
 	// write session end
+    console.log('ThinkGear Connector'.magenta + ' closed.'.grey);
+});
+
+
+
+/* Web Server */
+
+var app = express();
+
+
+/* Render index.html from public and log requests */
+app.configure(function() {
+	app.use(express.static(__dirname + '/public'));
+	app.use(express.logger('dev'));
+});
+
+
+/* Return brain data */
+app.get('/eeg.json', function(req, res) {
+
+	EegEvent.find({}, function (err, users) {
+		res.send(users);
+	});
+});
+
+
+/* Start web server */
+var server = app.listen(3000, function() {
+    console.log('Express.js'.magenta + ' listening on '.grey + '%d'.magenta, server.address().port);
 });
